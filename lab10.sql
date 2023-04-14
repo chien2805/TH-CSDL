ecbf8b08717edb6af35b925f9e8bfa3581b3b7c4
chien2805 <119844198+huynhvanchien123@users.noreply.github.com>
2023-04-14 16:15:57 +0700

    Add files via upload

---
+++ lab10.sql
@@ -0,0 +1,38 @@
+﻿1--A--
+INSERT INTO NhanVien (manv, tennv, gioitinh, diachi, sodt, email, phong)
+VALUES 
+('NV04', N'Nguyen Van A', 'Nam', 'Ha Noi', '0987654321', 'nva@example.com', N'Kế Tóan')
+
+
+BACKUP DATABASE QLBanHang TO DISK = 'C:\backup\QLBH.bak' 
+
+--B--
+INSERT INTO NhanVien (manv, tennv, gioitinh, diachi, sodt, email, phong)
+VALUES 
+('NV05', N'Nguyen Van A', 'Nam', 'Ha Noi', '0987654321', 'nva@example.com', N'Văn Phòng')
+
+BACKUP DATABASE QLBanHang TO DISK = 'C:\backup\QLBH_diff.bak' with differential
+
+--C--
+INSERT INTO NhanVien (manv, tennv, gioitinh, diachi, sodt, email, phong)
+VALUES 
+('NV06', N'Nguyen Van A', 'Nam', 'Ha Noi', '0987654321', 'nva@example.com', N'Tài Chính')
+
+BACKUP DATABASE QLBanHang TO DISK = 'C:\backup\QLBH_log1.trn' 
+
+--D--
+
+INSERT INTO NhanVien (manv, tennv, gioitinh, diachi, sodt, email, phong)
+VALUES 
+('NV07', N'Nguyen Van B', 'Nam', 'Ha Noi', '0987654321', 'nva@example.com', N'Tài Chính')
+
+BACKUP DATABASE QLBanHang TO DISK = 'C:\backup\QLBH_log1.trn' 
+
+
+--2--
+--a--
+drop database QLBanHang
+--b--
+restore database QLBanHang from disk ='C:\backup\QLBH.bak' 
+WITH STANDBY = 'C:\backup\QLBH_undoFile.undo';
+
